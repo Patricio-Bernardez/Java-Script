2ec19cc451ff72c93adeaf6e0bbaca8f46025c84
Patricio Bernardez <patriciobernardezestudios@gmail.com>
2023-10-02 15:05:04 -0300

    correcion 1 preentrega

--- main.js
+++ main.js
@@ -1,18 +1,40 @@
-let cuatrimestre1 = prompt("Ingresa la nota final del 1° cuatrimestre:");
-cuatrimestre1 = parseInt(cuatrimestre1)
-let cuatrimestre2 = prompt("Ingresa la nota final del 2° cuatrimestre:");
-cuatrimestre2 = parseInt(cuatrimestre2)
-let cuatrimestre3 = prompt("Ingresa la nota final del 3° cuatrimestre:");
-cuatrimestre3 = parseInt(cuatrimestre3);
-
-if (isNaN(cuatrimestre1) || isNaN(cuatrimestre2) || isNaN(cuatrimestre3)) {
-    console.log("Ingrese numeros validos del 1 al 10.");
-    alert("Ingrese numeros validos del 1 al 10.");
-} else {
-
-    let promedioFinal = (cuatrimestre1 + cuatrimestre2 + cuatrimestre3) / 3;
-
-
-    console.log("El promedio final de los tres cuatrimestres es: " + promedioFinal);
-    alert("El promedio final de los tres cuatrimestres es: " + promedioFinal);
+//Funciones
+function notaValida(nota) {
+    nota = parseInt(nota);
+    return !isNaN(nota) && nota >= 1 && nota <= 10;
 }
+
+function calcularPromedio(cuatrimestre1, cuatrimestre2, cuatrimestre3) {
+    return (cuatrimestre1 + cuatrimestre2 + cuatrimestre3) / 3;
+}
+let cuatrimestre1, cuatrimestre2, cuatrimestre3;
+
+//Ciclo
+do {
+    cuatrimestre1 = prompt("Ingresa la nota final del 1° cuatrimestre:");
+    if (!notaValida(cuatrimestre1)){
+        alert("Ingrese una nota del 1 al 10")
+    }
+} while (!notaValida(cuatrimestre1));
+
+do {
+    cuatrimestre2 = prompt("Ingresa la nota final del 2° cuatrimestre:");
+    if (!notaValida(cuatrimestre2)){
+        alert("Ingrese una nota del 1 al 10")
+    }
+} while (!notaValida(cuatrimestre2));
+
+
+do {
+    cuatrimestre3 = prompt("Ingresa la nota final del 3° cuatrimestre:");
+    if (!notaValida(cuatrimestre3)){
+        alert("Ingrese una nota del 1 al 10")
+    }
+} while (!notaValida(cuatrimestre3));
+
+let promedioFinal = calcularPromedio(parseInt(cuatrimestre1), parseInt(cuatrimestre2), parseInt(cuatrimestre3));
+
+promedioFinal = promedioFinal.toFixed(2);
+
+console.log("El promedio final de los tres cuatrimestres es: " + promedioFinal);
+alert("El promedio final de los tres cuatrimestres es: " + promedioFinal);
